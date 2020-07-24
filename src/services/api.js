71d4efa4f81e766d6d98b4c51cc88af12e04a4fc
pedrohcalado <pedrocalado22@gmail.com>
2020-07-24 17:04:13 -0300

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
-export async function getCategories() {
-  // implement here
+const fetch = require('node-fetch');
+
+// export 
+async function getCategories() {
+  return await fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' })
+  .then(response => response.json())
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+// export 
+async function getProductsFromCategoryAndQuery(categoryId, query) {
+  await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
+  .then(response => response.json())
+  // .then(data => console.log(data))
 }
+
+getCategories().then(data => {console.log(data)});
+
+getProductsFromCategoryAndQuery('MLB5672', 'porta');
