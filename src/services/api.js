236c5d255cdac8f5d1484eead5a77cce90203eab
pedrohcalado <pedrocalado22@gmail.com>
2020-07-24 17:49:46 -0300

    code climate

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 
 export async function getCategories() {
-  return await fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' })
-  .then(response => response.json())
-  .catch(error => console.log('Error'));
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' })
+  .then((response) => response.json())
+  .catch(() => console.log('Error'));
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
-  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
   .then(response => response.json())
-  .catch(error => console.log('Error'));
+  .catch(() => console.log('Error'));
 }
