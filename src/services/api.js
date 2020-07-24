041ca9f5786b97ee0f80e297abb5f1dc9b388e9c
César Alves <alvescesar@hotmail.com>
2020-07-24 18:59:51 -0300

    fetch API 3rd attempt

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
-  return fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' })
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
   .then((response) => response.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
   .then((response) => response.json());
 }
