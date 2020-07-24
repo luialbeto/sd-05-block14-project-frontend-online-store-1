6427b09a6420677cc013dbd31e307a09ba28da7d
pedrohcalado <pedrocalado22@gmail.com>
2020-07-24 17:43:49 -0300

    api function

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,13 @@
 const fetch = require('node-fetch');
 
-// export 
-async function getCategories() {
+export async function getCategories() {
   return await fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' })
   .then(response => response.json())
+  .catch(error => console.log('Error'));
 }
 
-// export 
-async function getProductsFromCategoryAndQuery(categoryId, query) {
-  await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
+  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, { method: 'GET' })
   .then(response => response.json())
-  // .then(data => console.log(data))
+  .catch(error => console.log('Error'));
 }
-
-getCategories().then(data => {console.log(data)});
-
-getProductsFromCategoryAndQuery('MLB5672', 'porta');
