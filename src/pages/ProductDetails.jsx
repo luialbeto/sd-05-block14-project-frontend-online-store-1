db3e56766aa7541b1e4d2f6a8a264615ef4ff07d
pedrohcalado <pedrocalado22@gmail.com>
2020-07-28 08:08:04 -0300

    code climate de novo

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 class ProductDetails extends React.Component {
   render() {
-    // console.log(JSON.parse(sessionStorage.getItem("items")).filter((item) => item.id === this.props.match.params.id));
-    const item = JSON.parse(sessionStorage.getItem('items')).filter((item) => item.id === this.props.match.params.id)[0];
-
+    const product = JSON.parse(sessionStorage.getItem('items'))
+    .filter((item) => item.id === this.props.match.params.id)[0];
     return (
       <div data-testid="product-detail-name">
-        {item.title}
+        {product.title}
       </div>
     );
   }
