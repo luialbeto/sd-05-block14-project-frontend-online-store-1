b2120f2c4a23933851a3877ecacee882dbb8c96b
luialbeto <araujo.script@gmail.com>
2020-07-29 07:58:31 -0300

    requisito 11 (atendido) com quebra api nos testes

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ProductDisplay extends React.Component {
-  constructor(props){
-    super(props);
-    this.transferToCart = this.transferToCart.bind(this)
-  }
-  transferToCart(event) {
-    console.log(event.target)
-    }
+ 
   render() {
     const { id, title, price, thumbnail } = this.props.product;
-    return (
-      <div data-testid="product" key={this.props.id}>
-        <img src={thumbnail} alt={title} />
-        <h4>{title}</h4>
-        <p batata={id}>{id}</p>
-        <p>{`R$ ${price}`}</p>
-        <Link data-testid="product-detail-link" to={`/product/${id}`} >VER DETALHES</Link>
-      </div>
+      return (
+      <section data-testid="product" key={this.props.id}>
+       <img src={thumbnail} alt={title} />
+       <h4>{title}</h4>
+         <p batata={id}>{id}</p>
+         <p>{`R$ ${price}`}</p>
+         <Link data-testid="product-detail-link" to={`/product/${id}`} >VER DETALHES</Link>
+       <button type="submit">Avaliar</button>
+      </section>
     );
   }
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,13 +1,66 @@
 import React from 'react';
 
 class ProductDetails extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        email: '',
+        mensagem: '',
+        rating: 0,
+      }
+      /* this.onHandleClick = this.onHandleClick.bind(this);
+      this.onHandleChange = this.onHandleChange.bind(this);
+      this.transferToCart = this.transferToCart.bind(this); */
+    }
+  
+     transferToCart(event) {
+      console.log(event.target)
+    }
+    
+/*     onHandleClick(rating) {
+      this.setState({ rating });
+    }
+  
+    onHandleChange(event) {
+      this.setState({ mensagem: event.target.value });
+    }
+  
+    onHandleClean() {
+      this.setState({ mensagem: '', rating: 0 });
+    } */
   render() {
+    const { email, mensagem, rating } = this.state;
     const product = JSON.parse(sessionStorage.getItem('items'))
     .filter((item) => item.id === this.props.match.params.id)[0];
     return (
+      <div>
       <div data-testid="product-detail-name">
         {product.title}
       </div>
+        <div>
+          <div>
+            <textarea
+              data-testid="product-detail-evaluation"
+              onChange={(event) => this.onHandleChange(event.target.value)}
+              placeholder="Submit"
+              value={mensagem}
+             />
+          </div>
+          <form onSubmit={this.submit}>
+          <input
+            id="email"
+            type="email"
+            onChange={(event) => this.setState(event.target.value)}
+            value={email}
+            />
+            </form>
+            <h1>
+            <p>{rating}</p>
+            </h1>
+            <div>
+             </div>
+      </div>
+    </div>
     );
   }
 }
